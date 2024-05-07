161780a93c35abd32042f32c6a1034f6f35ecae0
Sue Zadeh <suezadeh.a@gmail.com>
2024-05-08 04:07:38 +1200

    sql

--- scg_pa.sql
+++ scg_pa.sql
@@ -20,7 +20,6 @@ CREATE TABLE IF NOT EXISTS `sites` (
   `occupancy` INT NULL,
   PRIMARY KEY (`site_id`));
 
-
 -- -----------------------------------------------------
 -- Table `scg`.`bookings`
 -- -----------------------------------------------------
