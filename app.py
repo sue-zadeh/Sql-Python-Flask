a0ca089beea1869adb4748090fc124d7eee81b04
Sue Zadeh <suezadeh.a@gmail.com>
2024-05-09 02:01:11 +1200

    booking-customers-camper

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, flash
 from flask import render_template
 from flask import request
 from flask import redirect
@@ -58,7 +58,58 @@ def booking():
         siteList = connection.fetchall()
         return render_template("bookingform.html", customerlist = customerList, bookingdate=bookingDate, sitelist = siteList, bookingnights = bookingNights)    
 
+  # booking/add 
 @app.route("/booking/add", methods=['POST'])
 def makebooking():
-    print(request.form)
-    pass
+    site = request.form.get('site')
+    customer = request.form.get('customer')
+    booking_date = request.form.get('bookingdate')
+    occupancy = request.form.get('occupancy')
+    booking_nights = int(request.form.get('bookingnights'))
+
+    try:
+        connection = getCursor()
+        end_date = datetime.strptime(booking_date, '%Y-%m-%d') + timedelta(days=booking_nights)
+        query = """INSERT INTO bookings (site, customer, booking_date, end_date, occupancy)
+                   VALUES (%s, %s, %s, %s, %s)"""
+        connection.execute(query, (site, customer, booking_date, end_date.strftime('%Y-%m-%d'), occupancy))
+        connection.commit()  
+        flash('Booking successfully added!')
+        return redirect(url_for('booking'))
+    except mysql.connector.Error as err:
+        flash('Failed to add booking: {}'.format(err))
+        return redirect(url_for('booking'))
+    # print(request.form)
+    # pass
+    
+    # search customers
+@app.route("/search/customers", methods=['GET', 'POST'])
+def search_customers():
+    if request.method == 'POST':
+       search_query = request.form.get('search')
+       connection = getCursor()
+       query = "SELECT * FROM customers WHERE firstname LIKE %s OR familyname LIKE %s"
+       connection.execute(query, ('%' + search_query + '%', '%' + search_query + '%'))
+       results = connection.fetchall()
+       return render_template("search_results.html", results=results)
+    return render_template("search_customers.html")
+  
+  # add customers
+@app.route("/add/customer", methods=['GET', 'POST'])
+def add_customer():
+    if request.method == 'POST':
+      firstname = request.form.get('firstname')
+      familyname = request.form.get('familyname')
+      email = request.form.get('email')
+      phone = request.form.get('phone')
+      try:
+         connection = getCursor()
+         query = "INSERT INTO customers (firstname, familyname, email, phone) VALUES (%s, %s, %s, %s)"
+         connection.execute(query, (firstname, familyname, email, phone))
+         connection.commit()
+         flash('Customer successfully added!')
+         return redirect(url_for('add_customer'))
+      except mysql.connector.Error as err:
+         flash('Failed to add customer: {}'.format(err))
+         return render_template("add_customer.html")
+    return render_template("add_customer.html")   
