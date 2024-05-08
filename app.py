68d8d24311040e30b82b9d4af6b5a3cd35e12051
Sue Zadeh <suezadeh.a@gmail.com>
2024-05-09 10:19:45 +1200

    navbar

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ def getCursor():
     database=connect.dbname, autocommit=True)
     dbconn = connection.cursor()
     return dbconn
-if __name__ == "__main__":
-    app.run(debug=True)
+
 @app.route("/")
 def home():
     return render_template("base.html")
@@ -60,57 +59,48 @@ def booking():
         return render_template("bookingform.html", customerlist = customerList, bookingdate=bookingDate, sitelist = siteList, bookingnights = bookingNights)    
 
   # booking/add 
-@app.route("/booking/add", methods=['POST'])
 def makebooking():
     site = request.form.get('site')
     customer = request.form.get('customer')
     booking_date = request.form.get('bookingdate')
     occupancy = request.form.get('occupancy')
     booking_nights = int(request.form.get('bookingnights'))
-
+    cursor = getCursor()
+    end_date = datetime.strptime(booking_date, '%Y-%m-%d') + timedelta(days=booking_nights)
     try:
-        connection = getCursor()
-        end_date = datetime.strptime(booking_date, '%Y-%m-%d') + timedelta(days=booking_nights)
-        query = """INSERT INTO bookings (site, customer, booking_date, end_date, occupancy)
-                   VALUES (%s, %s, %s, %s, %s)"""
-        connection.execute(query, (site, customer, booking_date, end_date.strftime('%Y-%m-%d'), occupancy))
-        connection.commit()  
+        cursor.execute("INSERT INTO bookings (site, customer, booking_date, end_date, occupancy) VALUES (%s, %s, %s, %s, %s)", (site, customer, booking_date, end_date.strftime('%Y-%m-%d'), occupancy))
         flash('Booking successfully added!')
         return redirect(url_for('booking'))
     except mysql.connector.Error as err:
-        flash('Failed to add booking: {}'.format(err))
-        return redirect(url_for('booking'))
-    # print(request.form)
-    # pass
-    
-    # search customers
+        flash(f'Failed to add booking: {err}')
+        return redirect(url_for('bookingconfirmation.html'))
+
 @app.route("/search/customers", methods=['GET', 'POST'])
 def search_customers():
     if request.method == 'POST':
-       search_query = request.form.get('search')
-       connection = getCursor()
-       query = "SELECT * FROM customers WHERE firstname LIKE %s OR familyname LIKE %s"
-       connection.execute(query, ('%' + search_query + '%', '%' + search_query + '%'))
-       results = connection.fetchall()
-       return render_template("search_results.html", results=results)
-    return render_template("search_customers.html")
-  
-  # add customers
+        search_query = request.form.get('search')
+        cursor = getCursor()
+        cursor.execute("SELECT * FROM customers WHERE firstname LIKE %s OR familyname LIKE %s", ('%' + search_query + '%', '%' + search_query + '%'))
+        results = cursor.fetchall()
+        return render_template("search_results.html", results=results)
+    return render_template("searchcustomers.html")
+
 @app.route("/add/customer", methods=['GET', 'POST'])
 def add_customer():
     if request.method == 'POST':
-      firstname = request.form.get('firstname')
-      familyname = request.form.get('familyname')
-      email = request.form.get('email')
-      phone = request.form.get('phone')
-      try:
-         connection = getCursor()
-         query = "INSERT INTO customers (firstname, familyname, email, phone) VALUES (%s, %s, %s, %s)"
-         connection.execute(query, (firstname, familyname, email, phone))
-         connection.commit()
-         flash('Customer successfully added!')
-         return redirect(url_for('add_customer'))
-      except mysql.connector.Error as err:
-         flash('Failed to add customer: {}'.format(err))
-         return render_template("add_customer.html")
-    return render_template("addcustomer.html")   
+        firstname = request.form.get('firstname')
+        familyname = request.form.get('familyname')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        cursor = getCursor()
+        try:
+            cursor.execute("INSERT INTO customers (firstname, familyname, email, phone) VALUES (%s, %s, %s, %s)", (firstname, familyname, email, phone))
+            flash('Customer successfully added!')
+            return redirect(url_for('add_customer'))
+        except mysql.connector.Error as err:
+            flash(f'Failed to add customer: {err}')
+            return render_template("addcustomer.html")
+    return render_template("addcustomer.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
